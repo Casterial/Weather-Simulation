88fa27b7f7c881e1d00208ba54ff62a19887f6ac
Logan Shelton - Laptop <logan.shelton22@gmail.com>
2017-10-26 11:57:44 -0700

    Pushing Updated Functionality

--- Weather simulation/Assets/Scripts/RainSystem.cs
+++ Weather simulation/Assets/Scripts/RainSystem.cs
@@ -8,12 +8,8 @@ public class RainSystem : MonoBehaviour
     //http://answers.unity3d.com/questions/1135844/unity-5-turning-particles-onoff.html
     //http://answers.unity3d.com/questions/292745/how-do-i-create-instatiate-an-array-of-gameobjects.html
 
-
-    public GameObject RainZone1;
-    public GameObject RainZone2;
-
-    public ParticleSystem Rain01;
-    public ParticleSystem Rain02;
+    public GameObject[] RainZones;
+    public ParticleSystem[] RainParticles;
 
     // Use this for initialization
     void Start()
@@ -24,25 +20,37 @@ public class RainSystem : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Vector3.Distance(RainZone1.transform.position, RainZone2.transform.position) < 25.0f)
-        {
-            RainPlay();
-            Debug.Log("Rain is playing");
-        }
-        else
-        {
-            RainStop();
-        }
+        RainSystemFunction();
     }
 
-    void RainPlay()
+    void CloudMove()
     {
-        Rain01.Play();
-        Rain02.Play();
+
     }
-    void RainStop()
+
+    void RainSystemFunction()
     {
-        Rain01.Stop();
-        Rain02.Stop();
+        for (int i = 0; i < 2; i++)
+        {
+            ParticleSystem CurrRainZone = RainParticles[i];
+            for (int k = 0; k < 2; k++)
+            {
+                ParticleSystem OtherRainZone = RainParticles[k];
+                if (RainParticles[i] != RainParticles[k])
+                {
+                    if (Vector3.Distance(CurrRainZone.transform.position, OtherRainZone.transform.position) <= 25.0f)
+                    {
+                        CurrRainZone.Play();
+                        OtherRainZone.Play();
+                    }
+                    else
+                    {
+                        CurrRainZone.Stop();
+                        OtherRainZone.Stop();
+                    }
+                }
+            }
+        }
     }
 }
+
